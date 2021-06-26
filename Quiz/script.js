9207d02a3a4b1614c1b85f9c021843d5812a3f38
otoniel19 <otonielcostaoliveira3@gmail.com>
2021-06-25 23:41:10 -0300

    First commit

---
+++ Quiz/script.js
@@ -0,0 +1,10 @@
+function começar() {
+  let n1 = document.getElementsByName('nivel')[0]
+  
+  if(n1.checked) {
+    nivel1()
+  }
+}
+function nivel1() {
+  window.open("assets/pages/nivel1.html")
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# meus projetos
+
+https://otoniel19.github.io/projetos/
