c64e8f4b1834199cc04bf1b5e2ed17cbe175a7c3
otoniel19 <84187511+otoniel19@users.noreply.github.com>
2021-06-26 09:36:06 -0300

    update script.js

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,10 +1,12 @@
 function começar() {
   let n1 = document.getElementsByName('nivel')[0]
-  
-  if(n1.checked) {
+
+  if (n1.checked) {
     nivel1()
   }
 }
 function nivel1() {
-  window.open("assets/pages/nivel1.html")
+
+
+  window.open("assets/pages/nivel1.html")'
 }
